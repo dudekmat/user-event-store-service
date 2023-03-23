3c1368a7bb81cf252638b9c676517c5c937aa5ed
dudekmat <matdudek2706@gmail.com>
2023-03-23 23:25:06 +0100

    Fixed config

--- src/main/java/com/github/dudekmat/usereventstoreservice/config/ElasticsearchConfig.java
+++ src/main/java/com/github/dudekmat/usereventstoreservice/config/ElasticsearchConfig.java
@@ -4,7 +4,6 @@ import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import co.elastic.clients.json.jackson.JacksonJsonpMapper;
 import co.elastic.clients.transport.ElasticsearchTransport;
 import co.elastic.clients.transport.rest_client.RestClientTransport;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScope;
@@ -12,11 +11,12 @@ import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.elasticsearch.client.RestClient;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableConfigurationProperties(ElasticsearchProperties.class)
 @RequiredArgsConstructor
 class ElasticsearchConfig {
 
@@ -27,9 +27,10 @@ class ElasticsearchConfig {
 
     final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
     credentialsProvider.setCredentials(AuthScope.ANY,
-        new UsernamePasswordCredentials(elasticsearchProperties.getUsername(), elasticsearchProperties.getPassword()));
+        new UsernamePasswordCredentials(elasticsearchProperties.username(),
+            elasticsearchProperties.password()));
 
-    RestClient restClient = RestClient.builder(new HttpHost(elasticsearchProperties.getHost()))
+    RestClient restClient = RestClient.builder(new HttpHost(elasticsearchProperties.host()))
         .setHttpClientConfigCallback(httpClientBuilder ->
             httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider))
         .build();
--- src/main/java/com/github/dudekmat/usereventstoreservice/config/KafkaConsumerProperties.java
+++ src/main/java/com/github/dudekmat/usereventstoreservice/config/KafkaConsumerProperties.java
@@ -1,16 +1,9 @@
 package com.github.dudekmat.usereventstoreservice.config;
 
-import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
-@Component
 @ConfigurationProperties(prefix = "kafka.consumer")
-@Data
-class KafkaConsumerProperties {
+record KafkaConsumerProperties(String bootstrapServers, String groupId, String autoOffsetReset,
+                               Integer maxPollRecords) {
 
-  private String bootstrapServers;
-  private String groupId;
-  private String autoOffsetReset;
-  private Integer maxPollRecords;
 }
